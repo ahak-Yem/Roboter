50b17e94f8598d653c6a5207bcdaff7640666ffa
ahakDot <129846041+ahakDot@users.noreply.github.com>
2023-07-22 16:26:31 +0200

    Init CameraManager in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 #include "WifiManager.h"
+#include "CameraManager.h"
 
 const char* ssid = "Yemen";
 const char* password = "123456789";
-WifiManager wifiManager(ssid, password);
+WifiManager wifiManager(ssid, password);//init WifiManager
+CameraManager camManager; //init CameraManager
 
 void setup() {
     Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    delay(500);
+
+    if (camManager.initCamera()) {
+        Serial.println("Camera initialized");
+    } else {
+        Serial.println("Camera initialization failed");
+    }
+
     if (wifiManager.connect()) {
         Serial.println("Wi-Fi connected");
         Serial.print("IP address: ");
