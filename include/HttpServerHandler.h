e3e4c2017086079e7243dd00d3e88b12d57e0e88
@s0580976 <ahmed.kutbi@student.htw-berlin.de>
2023-08-02 23:06:32 +0200

    Add a moving average filter to smooth the stream

--- include/HttpServerHandler.h
+++ include/HttpServerHandler.h
@@ -4,6 +4,28 @@
 #include "esp_http_server.h"
 #include <Arduino.h>
 #include "MotorsManager.h"
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_timer.h"
+#include "img_converters.h"
+
+
+typedef struct {
+        size_t size; //number of values used for filtering
+        size_t index; //current value index
+        size_t count; //value count
+        int sum;
+        int * values; //array to be filled with values
+} ra_filter_t;
+
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+
+
+
 class HttpServerHandler{
     public:
     HttpServerHandler();
@@ -11,12 +33,16 @@ class HttpServerHandler{
 
     private:
     httpd_handle_t cameraHttpd = NULL;
-    httpd_handle_t streamHttpd = NULL; // Two servers for the stream and main one
+    httpd_handle_t streamHttpd = NULL; // Two servers for the stream and the html.
+    static ra_filter_t ra_filter;
 
     static MotorsManager motorManager;    
     static esp_err_t indexHandler(httpd_req_t *req); //handles frontend
     static esp_err_t streamHandler(httpd_req_t *req); //handles stream
     static esp_err_t cmdHandler(httpd_req_t *req); //handles commands comming
+    static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size_t len);
+    static ra_filter_t * ra_filter_init(ra_filter_t * filter, size_t sample_size); 
+    static int ra_filter_run(ra_filter_t * filter, int value);
 };
 
 #endif
--- src/HttpServerHandler.cpp
+++ src/HttpServerHandler.cpp
@@ -1,8 +1,5 @@
 #include "HttpServerHandler.h"
-#include "esp_camera.h"
-#include "Arduino.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
+
 
 //Constructor with a code to disable brownout detecting
 HttpServerHandler::HttpServerHandler()
@@ -19,6 +16,19 @@ MotorsManager HttpServerHandler::motorManager;
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
 static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+ra_filter_t HttpServerHandler::ra_filter;
+
+size_t HttpServerHandler::jpg_encode_stream(void * arg, size_t index, const void* data, size_t len){
+    jpg_chunking_t *j = (jpg_chunking_t *)arg;
+    if(!index){
+        j->len = 0;
+    }
+    if(httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK){
+        return 0;
+    }
+    j->len += len;
+    return len;
+}
 
 //A variable with HTML, CSS and JavaScript for frontpage style & actions capturing.
 static const char PROGMEM INDEX_HTML[] = R"rawliteral(
@@ -169,61 +179,77 @@ esp_err_t HttpServerHandler::indexHandler(httpd_req_t *req)
 
 //Handles the stream in a second stream server with different port
 esp_err_t HttpServerHandler::streamHandler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
+camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t * _jpg_buf = NULL;
+    char * part_buf[64];
 
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
-    return res;
-  }
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
 
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      res = ESP_FAIL;
-    } else {
-      if(fb->width > 400){
-        if(fb->format != PIXFORMAT_JPEG){
-          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-          esp_camera_fb_return(fb);
-          fb = NULL;
-          if(!jpeg_converted){
-            Serial.println("JPEG compression failed");
-            res = ESP_FAIL;
-          }
-        } else {
-          _jpg_buf_len = fb->len;
-          _jpg_buf = fb->buf;
-        }
-      }
-    }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    }
-    if(fb){
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      _jpg_buf = NULL;
-    } else if(_jpg_buf){
-      free(_jpg_buf);
-      _jpg_buf = NULL;
-    }
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
     if(res != ESP_OK){
-      break;
+        return res;
     }
-  }
-  return res;
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            Serial.printf("Camera capture failed");
+            res = ESP_FAIL;
+        } else {
+            if(fb->format != PIXFORMAT_JPEG){
+                bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+                esp_camera_fb_return(fb);
+                fb = NULL;
+                if(!jpeg_converted){
+                    Serial.printf("JPEG compression failed");
+                    res = ESP_FAIL;
+                }
+            } else {
+                _jpg_buf_len = fb->len;
+                _jpg_buf = fb->buf;
+            }
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(fb){
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        } else if(_jpg_buf){
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if(res != ESP_OK){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        uint32_t avg_frame_time = ra_filter_run(&ra_filter, frame_time);
+        Serial.printf("MJPG: %uB %ums (%.1ffps), AVG: %ums (%.1ffps)"
+            ,(uint32_t)(_jpg_buf_len),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time,
+            avg_frame_time, 1000.0 / avg_frame_time
+        );
+    }
+
+    last_frame = 0;
+    return res;
 }
 
 //Handles the commands that comes from the main server
@@ -272,6 +298,39 @@ if(!motorManager.controlCar(variable)){
   return httpd_resp_send(req, NULL, 0);
 }
 
+//Used to initialize a moving average filter to smooth out a stream of values.
+ra_filter_t *HttpServerHandler::ra_filter_init(ra_filter_t *filter, size_t sample_size)
+{
+    memset(filter, 0, sizeof(ra_filter_t));
+
+    filter->values = (int *)malloc(sample_size * sizeof(int));
+    if(!filter->values){
+        return NULL;
+    }
+    memset(filter->values, 0, sample_size * sizeof(int));
+
+    filter->size = sample_size;
+    return filter;
+}
+
+//Apply a moving average filter to a stream of values. 
+//It updates the filter's internal circular buffer with new values, 
+//removes the oldest value, and calculates the average of the current values in the buffer. 
+int HttpServerHandler::ra_filter_run(ra_filter_t *filter, int value)
+{
+  if(!filter->values){
+    return value;
+  }
+  filter->sum -= filter->values[filter->index];
+  filter->values[filter->index] = value;
+  filter->sum += filter->values[filter->index];
+  filter->index++;
+  filter->index = filter->index % filter->size;
+  if (filter->count < filter->size) {
+    filter->count++;
+  }
+  return filter->sum / filter->count;
+}
 
 void HttpServerHandler::startServer()
 {
@@ -296,6 +355,9 @@ void HttpServerHandler::startServer()
     .handler   = streamHandler,
     .user_ctx  = NULL
   };
+  
+  ra_filter_init(&ra_filter, 20);
+  Serial.printf("Starting web server on port: '%d'", config.server_port);
   if (httpd_start(&cameraHttpd, &config) == ESP_OK) {
     httpd_register_uri_handler(cameraHttpd, &index_uri);
     httpd_register_uri_handler(cameraHttpd, &cmd_uri);
